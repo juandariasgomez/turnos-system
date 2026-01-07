f85a8ec5b1d5a02150d01c1d3b69c5ac0d2c62db
Juan David A.G <juandariasgomez@gmail.com>
2026-01-06 21:40:33 -0500

    Update with out warnings

--- src/main/java/com/juand/turnosbackend/ticket/TicketService.java
+++ src/main/java/com/juand/turnosbackend/ticket/TicketService.java
@@ -4,11 +4,13 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.juand.turnosbackend.api.dto.BoardItemDTO;
 import com.juand.turnosbackend.api.dto.TicketResponseDTO;
 import com.juand.turnosbackend.person.Person;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -34,11 +36,11 @@ public class TicketService {
       String code = prefix + String.valueOf(counter.incrementAndGet());
       try {
         Ticket t = Ticket.builder()
-          .code(code)
-          .status(TicketStatus.CREATED)
-          .createdAt(Instant.now())
-          .person(person)
-          .build();
+            .code(code)
+            .status(TicketStatus.CREATED)
+            .createdAt(Instant.now())
+            .person(person)
+            .build();
         return buildTicketResponseDTO(ticketRepository.save(t));
       } catch (DataIntegrityViolationException ex) {
         // retry witch other code
@@ -47,24 +49,26 @@ public class TicketService {
   }
 
   public TicketResponseDTO callNext(final int moduleNumber) {
-    Ticket t = ticketRepository.findFirstByStatusOrderByCreatedAtAsc(TicketStatus.CREATED).orElseThrow(() -> new ResponseStatusException(
-      HttpStatus.NOT_FOUND, "No hay tickets pendientes"));
+    Ticket t = ticketRepository.findFirstByStatusOrderByCreatedAtAsc(TicketStatus.CREATED)
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "No hay tickets pendientes"));
     t.setStatus(TicketStatus.CALLED);
     t.setCalledAt(Instant.now());
     t.setModuleNumber(moduleNumber);
     t = ticketRepository.save(t);
     // Hacer lógica para enviar al websocket el ticket llamado.
-    BoardItemDTO dto = new BoardItemDTO(t.getCode(), t.getModuleNumber(), t.getPerson().getName() + " " + t.getPerson().getLastName());
+    BoardItemDTO dto = new BoardItemDTO(t.getCode(), t.getModuleNumber(),
+        t.getPerson().getName() + " " + t.getPerson().getLastName());
     ws.convertAndSend("/topic/board", dto);
-    return buildTicketResponseDTO(t);
+    return buildTicketResponseDTO(Objects.requireNonNull(t));
   }
 
   public TicketResponseDTO serve(Long id) {
     if (id == null) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "ID no puede ser nulo");
     }
-    Ticket t =
-      ticketRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No se ha encontrado el ticket"));
+    Ticket t = ticketRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No se ha encontrado el ticket"));
     t.setStatus(TicketStatus.SERVED);
     t.setServedAt(Instant.now());
     Map<String, Object> payload = new HashMap<>();
@@ -75,12 +79,21 @@ public class TicketService {
 
   public List<BoardItemDTO> lastCalled(final int limit) {
     return ticketRepository.findTop10ByStatusOrderByCalledAtDesc(TicketStatus.CALLED).stream().limit(limit)
-      .map(t -> new BoardItemDTO(t.getCode(), t.getModuleNumber(), t.getPerson().getName() + " " + t.getPerson().getLastName())).toList();
+        .map(t -> new BoardItemDTO(t.getCode(), t.getModuleNumber(),
+            t.getPerson().getName() + " " + t.getPerson().getLastName()))
+        .toList();
   }
 
-  private TicketResponseDTO buildTicketResponseDTO(final Ticket ticket) {
-    return new TicketResponseDTO(ticket.getId(), ticket.getCode(), ticket.getStatus(), ticket.getCreatedAt(),
-      ticket.getCalledAt(), ticket.getServedAt(), ticket.getModuleNumber() != null ? ticket.getModuleNumber() : 0,
-      ticket.getPerson().getName() + " " + ticket.getPerson().getLastName());
+  private TicketResponseDTO buildTicketResponseDTO(Ticket ticket) {
+    return new TicketResponseDTO(
+        ticket.getId(),
+        ticket.getCode(),
+        ticket.getStatus(),
+        ticket.getCreatedAt(),
+        ticket.getCalledAt(),
+        ticket.getServedAt(),
+        ticket.getModuleNumber() != null ? ticket.getModuleNumber() : 0,
+        ticket.getPerson().getName() + " " + ticket.getPerson().getLastName());
   }
+
 }
